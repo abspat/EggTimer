b3386b45bae3f7b01e1e9997eda6fd287a14e4cc
Abhay Patel <abhay.patel@rocketmail.com>
2021-02-19 18:35:44 -0500

    Added button functionality

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    @IBOutlet weak var labelOutput: UILabel!
+    
+    var eggTimes = ["Soft": 3, "Medium":5, "Hard":10]
+    
+    var timer = Timer()
+    var startTime = 0
+    var totalTime = 0
+   
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        timer.invalidate()
+        startTime = 0
+        
+        let hardness = sender.currentTitle!
+        progressView.progress = 0.0
+        
+        totalTime = eggTimes[hardness]!
+        
+        labelOutput.text = hardness
+        
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    
+     
+}
+
+    @objc func updateCounter() {
+        //example functionality
+        if startTime <= totalTime  {
+            
+            progressView.progress = Float(startTime)/Float(totalTime)
+            startTime += 1
+        }
+        
+        else {
+            labelOutput.text = "Done!"
+            
+            timer.invalidate()
+        }
+        
+        
+        
     }
-
-
+    
+    
+    
 }
 
